f6dfd5fa734bb4a61e2b949fb7a091a8b239843c
EllaKaye <ella.kaye@gmail.com>
2025-06-25 12:51:02 +0100

    rep.c

--- src/rep.c
+++ src/rep.c
@@ -2,3 +2,18 @@
 #include <R.h>
 #include <Rinternals.h>
 
+SEXP c_rle(SEXP x, SEXP times) {
+
+  size_t length = XLENGTH(x) * Rf_asReal(times);
+
+  SEXP out = PROTECT(Rf_allocVector(REALSXP, length));
+
+  // for now, assume length(x) = 1
+  for (size_t i = 0; i < length; ++i) {
+    REAL(out)[i] = Rf_asReal(x);
+  }
+
+  UNPROTECT(1);
+
+  return out;
+}
