91bad5ab2d9739680b4822b2f04d23009cbea330
EllaKaye <ella.kaye@gmail.com>
2025-06-26 12:17:30 +0100

    Works with vec x and single times

--- R/rep.R
+++ R/rep.R
@@ -13,5 +13,26 @@
 rep_c <- function(x, times = 1) {
   # Do some checking on the arguments
 
+  if (is.integer(x)) {
+    x <- as.numeric(x)
+  }
+
   .Call(c_rep, x, times)
+}
+
+rep_check <- function(x, times = 1) {
+  length_x <- length(x)
+
+  for (i in (seq_len(times) - 1)) {
+    for (j in (seq_along(x) - 1)) {
+      print(paste(
+        "i: ",
+        i,
+        "j:",
+        j,
+        "i * length_x + j:",
+        i * length_x + j
+      ))
+    }
+  }
 }
--- src/rep.c
+++ src/rep.c
@@ -1,16 +1,28 @@
+#include <stddef.h>
 #define R_NO_REMAP
 #include <R.h>
 #include <Rinternals.h>
 
+
+// TODO: add `each` arg
+// MAYBE: add `length.out` arg
 SEXP c_rep(SEXP x, SEXP times) {
 
-  size_t length = XLENGTH(x) * Rf_asReal(times);
+  // for now assume times is a single numeric
+  // TODO: allow times to be a vector
+  int times_c = Rf_asInteger(times);
+  size_t length_x = XLENGTH(x);
+  size_t length_out = length_x * times_c;
+  const double *xp = REAL(x);
 
-  SEXP out = PROTECT(Rf_allocVector(REALSXP, length));
+  SEXP out = PROTECT(Rf_allocVector(REALSXP, length_out));
 
   // for now, assume length(x) = 1
-  for (size_t i = 0; i < length; ++i) {
-    REAL(out)[i] = Rf_asReal(x);
+  for (size_t i = 0; i < times_c; ++i) {
+    for (size_t j = 0; j < length_x; j++) {
+      REAL(out)[i * length_x + j] = xp[j];
+      //REAL(out)[i] = xp[0];
+    }
   }
 
   UNPROTECT(1);
