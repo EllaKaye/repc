461f4d46b9cf9ec3c04adc35b70fe3b5d5a67f11
EllaKaye <ella.kaye@gmail.com>
2025-07-01 15:13:40 +0100

    Document `each` arg

--- R/rep.R
+++ R/rep.R
@@ -3,7 +3,8 @@
 #' `rep_c()` repeats a vector
 #'
 #' @param x A single numeric value
-#' @param times A single numeric value
+#' @param times An integer vector
+#' @param each A single integer value
 #'
 #' @examples
 #' rep_c(1, 3)
