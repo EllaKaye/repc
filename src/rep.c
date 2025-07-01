62c1e2ddae44d7e1b27a6dc2f7fe83064634c7a9
EllaKaye <ella.kaye@gmail.com>
2025-07-01 14:51:50 +0100

    Deal with each > 1 (as long as times = 1)

--- src/rep.c
+++ src/rep.c
@@ -13,20 +13,30 @@ SEXP c_rep(SEXP x, SEXP times, SEXP each) {
   int times_c = Rf_asInteger(times);
   int each_c = Rf_asInteger(each);
   size_t length_x = XLENGTH(x);
+  size_t length_times = XLENGTH(times);
   size_t length_out = length_x * times_c * each_c;
   const double *xp = REAL(x);
 
   SEXP out = PROTECT(Rf_allocVector(REALSXP, length_out));
 
-  // for now, assume length(x) = 1
-  for (size_t i = 0; i < times_c; ++i) {
-    for (size_t j = 0; j < length_x; j++) {
-      REAL(out)[i * length_x + j] = xp[j];
-      //REAL(out)[i] = xp[0];
+  if (each_c == 1 && length_times == 1) {
+    for (size_t i = 0; i < times_c; ++i) {
+      for (size_t j = 0; j < length_x; j++) {
+        REAL(out)[i * length_x + j] = xp[j];
+      }
     }
+  } else if (each_c > 1 && times_c == 1 && length_times == 1) {
+    for (size_t i = 0; i < length_x; ++i) {
+      for (size_t j = 0; j < each_c; j++) {
+        REAL(out)[i * each_c + j] = xp[i];
+      }
+    }
+  } else {
+    printf("Haven't dealt with this case yet");
   }
 
   UNPROTECT(1);
 
   return out;
 }
+
